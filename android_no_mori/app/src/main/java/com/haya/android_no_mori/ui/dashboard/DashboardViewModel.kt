a73dc933dc16e5bd9526af7897e6afeae0b1e35b
shinji hayamizu <max9173627@gmail.com>
2021-12-26 21:30:11 +0900

    first commit

---
+++ android_no_mori/app/src/main/java/com/haya/android_no_mori/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.haya.android_no_mori.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ android_no_mori/app/src/main/java/com/haya/android_no_mori/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.haya.android_no_mori.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
