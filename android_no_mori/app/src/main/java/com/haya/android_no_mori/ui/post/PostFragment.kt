0eb4c9374ff695d9a6466270c0e01315f0365551
shinji hayamizu <max9173627@gmail.com>
2022-03-17 21:24:44 +0900

    投稿用レイアウトの作成

--- android_no_mori/app/src/main/java/com/haya/android_no_mori/ui/post/PostFragment.kt
+++ android_no_mori/app/src/main/java/com/haya/android_no_mori/ui/post/PostFragment.kt
@@ -4,37 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.haya.android_no_mori.databinding.FragmentHomeBinding
 
 class PostFragment : Fragment() {
-
     private lateinit var homeViewModel: PostViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
             ViewModelProvider(this).get(PostViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
